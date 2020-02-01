72b75fa4f0b75019ab6a3d79985a4e07d2eadd18
zhao828 <490701019@qq.com>
2020-01-31 19:27:28 -0800

    first version

---
+++ sell/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <v-header></v-header>
+    <div class="tab">
+      <div class="tab-item">
+        <router-link to="./goods">商品</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="./ratings">评价</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="./seller">商家</router-link>
+      </div>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import header from './components/header/header'
+export default {
+  name: 'App',
+  components: {
+      'v-header': header
+  }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  #app
+    .tab
+      display:flex
+      width: 100%
+      hight: 40px
+      line-height: 40px
+      .tab-item
+        flex:1
+        text-align: center
+        & > a
+          display block
+          font-size  14px
+          color: rgb(77,85,93)
+          &.active
+            color:rgb(240,20,20)
+
+</style>
---
+++ sell/src/components/header/header.vue
@@ -0,0 +1,13 @@
+<template>
+<div>woshiheader</div>
+</template>
+
+<script>
+export default {
+  name: 'header'
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
